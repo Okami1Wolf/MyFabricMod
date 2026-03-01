03f5cae7f221117634b581341d2e1f9dad30fb98
Okami1Wolf <zgunira1@gmail.com>
2026-03-01 23:21:09 +0500

    Update ModMain.java

--- src/main/java/com/example/myfabricmod/ModMain.java
+++ src/main/java/com/example/myfabricmod/ModMain.java
@@ -1,10 +1,22 @@
 package com.example.myfabricmod;
 
+import com.example.myfabricmod.chat.ChatConfig;
+import com.example.myfabricmod.chat.ChatManager;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
 public class ModMain implements ModInitializer {
+
     @Override
     public void onInitialize() {
         System.out.println("MyFabricMod loaded!");
+    }
+
+    public static class Client implements ClientModInitializer {
+        @Override
+        public void onInitializeClient() {
+            new ChatConfig().onInitializeClient();
+            new ChatManager().onInitializeClient();
+        }
     }
 }
