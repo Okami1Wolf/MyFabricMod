a10a9723fb12331f22cbfd7da93aff2245d7b07f
Okami1Wolf <zgunira1@gmail.com>
2026-03-01 23:04:51 +0500

    Create ModMain.java

---
+++ src/main/java/com/example/myfabricmod/ModMain.java
@@ -0,0 +1,10 @@
+package com.example.myfabricmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("MyFabricMod loaded!");
+    }
+}
